c39f1663e90e8738389e0f85783d111cf06716c7
Felipe Souza <fesouza2345@gmail.com>
2023-06-06 10:27:48 -0300

    .

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env["STOCK_API"],
+  uri: process.env.STOCK_API,
   cache: new InMemoryCache({}),
   credentials: "include",
 });
